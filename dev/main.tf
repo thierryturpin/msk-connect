03e241f6b8a0fe9dbe849b68bd10c3127bd7c4cf
thierryturpin <thierry@turpin.be>
2021-09-24 12:29:48 +0200

    wip version, change region to central-1 ( issue with KMS keys )

--- dev/main.tf
+++ dev/main.tf
@@ -14,7 +14,7 @@ terraform {
 }
 
 provider "aws" {
-  region                  = "eu-west-1"
+  region                  = "eu-central-1"
   shared_credentials_file = "~/.aws/credentials"
   profile                 = "tf-turpin"
 
