13bcfcccf9998f8a7381190eed8f54e03ca54c1e
thierryturpin <thierry@turpin.be>
2021-09-24 12:30:34 +0200

    wip version, use vpc subnets, change descriptio of aws_kms_key to check!

--- dev/msk.tf
+++ dev/msk.tf
@@ -20,24 +20,6 @@ module "sg_msk_private_kafka_rule" {
   number_of_computed_ingress_with_source_security_group_id = 1
 }
 
-# Could be replaced by filter on tag "Type" = "private"
-data "aws_subnet_ids" "private_subnet_ids" {
-  vpc_id = module.vpc_dev.vpc_id
-  tags = {
-    Name = "${var.env}-private-eu-west-*"
-  }
-}
-
-data "aws_subnet" "private_subnets" {
-  count = length(data.aws_subnet_ids.private_subnet_ids.ids)
-  id    = tolist(data.aws_subnet_ids.private_subnet_ids.ids)[count.index]
-}
-
-output "private_subnet_ids" {
-  value = [data.aws_subnet.private_subnets.*.id]
-}
-
-
 resource "aws_msk_cluster" "msk" {
   cluster_name           = "${var.env}-msk"
   kafka_version          = "2.6.2"
@@ -46,7 +28,7 @@ resource "aws_msk_cluster" "msk" {
   broker_node_group_info {
     instance_type   = "kafka.t3.small"
     ebs_volume_size = 50
-    client_subnets  = data.aws_subnet.private_subnets.*.id
+    client_subnets  = module.vpc_dev.private_subnets
     security_groups = [aws_security_group.sg_msk_private.id]
   }
   client_authentication {
@@ -78,7 +60,8 @@ resource "aws_secretsmanager_secret" "connect" {
 }
 
 resource "aws_kms_key" "connect" {
-  description = "Connect user key for MSK Cluster Scram Secret Association"
+  description = "Connect user key for MSK Cluster Scram Secret association"
+  deletion_window_in_days = 7
 }
 
 resource "aws_secretsmanager_secret_version" "connect" {
